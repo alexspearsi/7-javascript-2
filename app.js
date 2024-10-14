de8b724fd04a55734e3770afcc99de82c0fae1a8
Alex <strelchenko.aleksandr@yandex.ru>
2024-10-14 10:34:33 +0300

    commit attributes
    description 123

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+console.log(elem.getAttribute('About'));
+elem.setAttribute('Test', 123);
+console.log(elem.outerHTML);
+
+for (let attr of elem.attributes) {
+    console.log(`${attr.name} ${attr.value}`);
+}
+
+
+console.log(elem.attributes
+    
+);
