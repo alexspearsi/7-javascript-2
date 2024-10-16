d210d78266f8bd2a321a9a77c85a5d0ad05ba0f9
Alex <strelchenko.aleksandr@yandex.ru>
2024-10-16 23:19:10 +0300

    commit commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let check = "it's ready"
+const operations = [100, -20, 7б -30, 50];
+
+const minElement = operations.reduce((acc, operation, i) => {
+    if (operation > acc) {
+        return acc;
+    }
+})
