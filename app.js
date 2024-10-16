71e830f73d9545bb9a8d9150523a53c2c5c7e4d6
Alex <strelchenko.aleksandr@yandex.ru>
2024-10-17 00:26:49 +0300

    commit 8

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const operations = [100, -20, 7б -30, 50];
+const arr = [2, 4, 4, 10];
 
-const minElement = operations.reduce((acc, operation, i) => {
-    if (operation > acc) {
-        return acc;
+const avg = arr.reduce((acc, el, i) => {
+    if (i != arr.length - 1) {
+        return acc + el;
+    } else {
+         return (acc +el) / arr.length;
     }
-})
+}, 0)
+
+console.log(avg);
