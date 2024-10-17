f802435979cb690e06072a86df811261f19a2c02
Alex <strelchenko.aleksandr@yandex.ru>
2024-10-17 10:17:16 +0300

    feat: add new features

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-let string = 'and'
+function showNotification({top = 0, right = 0, className, html}) {
+
+    let notification = document.createElement('div');
+    notification.className = "notification";
+    if (className) {
+      notification.classList.add(className);
+    }
+    notification.style.position = 'absolute';
+    notification.style.top = top +'px';
+    notification.style.right = right + 'px';
+
+    notification.innerHTML = html;
+    document.body.append(notification);
+
+    setTimeout(() => notification.remove(), 1000);
+  }
+
+  // test it
+  let i = 1;
+  setInterval(() => {
+    showNotification({
+      top: 10,
+      right: 10,
+      html: 'Hello ' + i++,
+      className: "welcome"
+    });
+  }, 2000);
