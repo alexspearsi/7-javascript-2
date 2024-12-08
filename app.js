2d016441c7fa8ff41caa3a2caca9fdbea10374ca
Alex <strelchenko.aleksandr@yandex.ru>
2024-12-08 13:50:13 +0200

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function getCoffees() {
-    document.querySelector('div').textContent = '';
-    fetch('./coffees.json')
+ document.querySelector('div').textContent = '';
+  fetch('./coffees.json')
         .then(response => response.json())
-        .then(response => {
-            response.forEach(element =>
+    .then(response => {
+          response.forEach(element =>
                 document.querySelector('div').innerHTML += `<h1>${element.name} = ${element.price}</h1>`
             )
-        })
+    })
 } 
